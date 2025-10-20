d08171a1e3254365a2f192a190e9e61b63df02e3
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-20 17:48:19 -0500

    feat(repositories): add base repository and unit of work interfaces for repository pattern implementation.

---
+++ CatchUpPlatform.API/Shared/Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,10 @@
+namespace CatchUpPlatform.API.Shared.Domain.Repositories;
+
+public interface IBaseRepository<TEntity>
+{
+    Task AddAsync(TEntity entity);
+    Task<TEntity?> FindByIdAsync(int id);
+    Task<IEnumerable<TEntity>> ListAsync();
+    void Update(TEntity entity);
+    void Remove(TEntity entity);
+}
